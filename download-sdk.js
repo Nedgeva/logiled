c469e860e478965c0adb4feb0f06952f3337fa01
Pavel Udalov <pavel.udalov@devexperts.com>
2019-12-03 22:43:53 +0400

    fix dependencies
    Removed junk deps, upgraded to last versions
    Fixed building against fresh versions of node

--- download-sdk.js
+++ download-sdk.js
@@ -1,46 +1,29 @@
+const LOGITECH_LED_SDK_URL = "https://gaming.logitech.com/sdk/LED_8.87.zip";
 
-const LOGITECH_LED_SDK_URL = 'https://gaming.logitech.com/sdk/LED_8.87.zip';
-
-var fs = require('fs');
-var path = require('path');
-var EOL = require('os').EOL;
-var unzip = require('unzip');
-var fstream = require('fstream');
-var request = require('request');
-var progress = require('request-progress');
-var progressBar = require('progress-bar').create(process.stdout, (process.stdout.columns || 70) - 20);
-progressBar.format = '$bar; $percentage;% finished.';
-
-var writeStream = new fstream.Writer(__dirname);
+var fs = require("fs");
+var path = require("path");
+var EOL = require("os").EOL;
+var unzip = require("unzipper");
+var request = require("request");
+var progressBar = require("progress-bar").create(
+  process.stdout,
+  (process.stdout.columns || 70) - 20
+);
+progressBar.format = "$bar; $percentage;% finished.";
 
 // only download SDK if not already done
-if (!fs.existsSync(path.join(__dirname, 'LED'))) {
+if (!fs.existsSync(path.join(__dirname, "LED"))) {
+  // download and unzip SDK into the LED folder...
+  console.log("Download and unzip Logitech LED SDK...");
 
-    // download and unzip SDK into the LED folder...
-    console.log('Download and unzip Logitech LED SDK...');
-
-    // also show a fancy progress bar if stdout is a terminal
-    progress(request(LOGITECH_LED_SDK_URL), {
-        throttle: 250
+  // also show a fancy progress bar if stdout is a terminal
+  request(LOGITECH_LED_SDK_URL)
+    .on("error", function(err) {
+      console.log("error downloading Logitech LED SDK:", err.message, EOL);
+      process.exit(1);
     })
-    .on('progress', function (state) {
-        if (process.stdout.isTTY) {
-            progressBar.update(state.percent);
-        }
+    .on("end", function() {
+      console.log("finished successfully." + EOL);
     })
-    .on('error', function (err) {
-        if (process.stdout.isTTY) {
-            progressBar.clear();
-        }
-        console.log('error downloading Logitech LED SDK:', err.message, EOL);
-        process.exit(1);
-    })
-    .on('end', function (state) {
-        if (process.stdout.isTTY) {
-            progressBar.clear();
-        }
-        console.log('finished successfully.' + EOL);
-    })
-    .pipe(unzip.Parse())
-    .pipe(writeStream);
+    .pipe(unzip.Extract({ path: __dirname }));
 }
