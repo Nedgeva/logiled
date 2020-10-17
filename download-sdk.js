ae0f665b56797f717fe2b92035bcd5ad27b251c5
Pavel Udalov <pavel.udalov@devexperts.com>
2020-10-18 00:24:10 +0400

    Fixed download

--- download-sdk.js
+++ download-sdk.js
@@ -1,29 +1,43 @@
-const LOGITECH_LED_SDK_URL = "https://gaming.logitech.com/sdk/LED_9.00.zip";
+const LOGITECH_LED_SDK_URL = "https://www.logitechg.com/sdk/LED_SDK_9.00.zip";
 
-const fs = require("fs");
+const fs = require("fs/promises");
 const path = require("path");
-const EOL = require("os").EOL;
-const unzip = require("unzipper");
-const request = require("request");
-const progressBar = require("progress-bar").create(
-  process.stdout,
-  (process.stdout.columns || 70) - 20
-);
-progressBar.format = "$bar; $percentage;% finished.";
+const axios = require("axios");
+const ProgressBar = require("progress");
+const unzip = require("unzip-stream");
 
-// only download SDK if not already done
-if (!fs.existsSync(path.join(__dirname, "LED"))) {
-  // download and unzip SDK into the LED folder...
-  console.log("Download and unzip Logitech LED SDK...");
+async function downloadSDK() {
+  try {
+    const stats = await fs.stat("./LED");
+    if (stats.isDirectory())
+      return console.log("Download aborted, resources already in LED folder");
+  } catch (error) {}
 
-  // also show a fancy progress bar if stdout is a terminal
-  request(LOGITECH_LED_SDK_URL)
-    .on("error", function(err) {
-      console.log("error downloading Logitech LED SDK:", err.message, EOL);
-      process.exit(1);
-    })
-    .on("end", function() {
-      console.log("finished successfully." + EOL);
-    })
-    .pipe(unzip.Extract({ path: __dirname }));
+  console.log("Connecting …");
+
+  const { data, headers } = await axios({
+    url: LOGITECH_LED_SDK_URL,
+    method: "GET",
+    responseType: "stream",
+  });
+
+  const totalLength = headers["content-length"];
+
+  console.log("Starting download");
+
+  const progressBar = new ProgressBar("-> downloading [:bar] :percent :etas", {
+    width: 40,
+    complete: "=",
+    incomplete: " ",
+    renderThrottle: 1,
+    total: parseInt(totalLength),
+  });
+
+  data
+    .on("data", (chunk) => progressBar.tick(chunk.length))
+    .on("error", console.error);
+
+  data.pipe(unzip.Extract({ path: __dirname }));
 }
+
+downloadSDK();
